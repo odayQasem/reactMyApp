01ec4564ae1168d142ae2d1a9efa3af4bf6cd45b
Oday qasem <oday.qasim@gmail.com>
2019-03-27 17:18:28 +0200

    first

---
+++ build/precache-manifest.062085c558614d7416561c7ee0ba4f34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "320c77eb5143197d7eee",
+    "url": "/static/js/main.320c77eb.chunk.js"
+  },
+  {
+    "revision": "9ca68313a3e644e28403",
+    "url": "/static/js/2.9ca68313.chunk.js"
+  },
+  {
+    "revision": "320c77eb5143197d7eee",
+    "url": "/static/css/main.bd57d500.chunk.css"
+  },
+  {
+    "revision": "9ca68313a3e644e28403",
+    "url": "/static/css/2.231b727b.chunk.css"
+  },
+  {
+    "revision": "2cf892f911c7881f529a6b3f506a73d8",
+    "url": "/index.html"
+  }
+];
